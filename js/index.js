bb35844e1b1148b8efd5d8de7d4fddf4dd8def6c
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-02-25 13:39:57 -0300

    cambios para cumplir con requisitos del proyecto

--- js/index.js
+++ js/index.js
@@ -1,12 +1,24 @@
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-    $('#contacto').on('shown.bs.modal', function(e) {
-        $('.btn-info-modal').addClass('btn-info-modal-open')
-        $('.btn-info-modal').prop('disabled', true)
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $('#contacto').on('show.bs.modal', function (e) {
+        console.log("El modal se está mostrando");
     })
-    $('#contacto').on('hidden.bs.modal', function(e) {
+
+    $('#contacto').on('shown.bs.modal', function (e) {
+        console.log("El modal se mostró");
+
+        $('.btn-info-modal').addClass('btn-info-modal-open');
+        $('.btn-info-modal').prop('disabled', true);
+    })
+    $('#contacto').on('hide.bs.modal', function (e) {
+        console.log("El modal se está ocultando")
+    })
+    $('#contacto').on('hidden.bs.modal', function (e) {
+        console.log("El modal se ocultó")
+
         $('.btn-info-modal').removeClass('btn-info-modal-open')
         $('.btn-info-modal').prop('disabled', false)
     })
-    $('.carousel').carousel({interval: 2000})
+    $('.carousel').carousel({ interval: 2000 })
 })
